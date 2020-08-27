3967bdf95aac66cc94abb540b00957c540600743
Willem Stolte <Willem.Stolte@deltares.nl>
2020-08-27 16:55:24 +0200

    small change

--- tests.R
+++ tests.R
@@ -6,70 +6,54 @@ require(sf)
 source("app2/functionality.r")
 
 ncdf4::nc_close(nc = nc)
+
+#test with north sea model output
 con <- "p:/11204882-002-interreg-wadden-sea/simulations/A07_waq_normal_e3_2006_new_obs/DFM_OUTPUT_DCSM-FM_0_5nm_waq/DCSM-FM_0_5nm_waq_0000_his.nc"
-con <- "c:\\Users\\stolte\\Documents\\testdata\\MB_02_waq_0000_his.nc"
 nc <- ncdf4::nc_open(con)
-names(nc$var)
+vars = c("NO3", "NH4")
+vars = "temperature"
+station_id = c("NOORDWK20", "NOORDWK70")
+layer = 1
+start = 1
+end = NULL
+#
+time = ncvar_get(nc, "time")
+ncvar_get(nc, "NO3", start = c(1,165,1), count = c(1,1,-1)) %>% dim()
+t_1 <- ncvar_get(nc, "temperature", start = c(1,217,1), count = c(1,1,-1))
+t_20 <- ncvar_get(nc, "temperature", start = c(20,217,1), count = c(1,1,-1))
+df <- bind_cols(time = time, t1 = t_1, t20 = t_20)
 
-timeUnits <- nc$dim$time$units
-timeOffset <- sub("seconds since ", "", nc$dim$time$units)
+ggplot(df) +
+  geom_line(aes(time, y = t1)) +
+  geom_line(aes(time, y = t20), color = "red")
 
-nc$var$Si$size
-
-# first dimension: layer
-# second dimension: location
-# third dimension: timestep
-
-allVars <- map(nc$var, list("size"))
-
-allVarsDims <- map(allVars, function(x) length(x)) %>% 
-  unlist(use.names = T) %>% 
-  as.data.frame() %>%
-  rownames_to_column() %>%
-  rename(variable = rowname, dims = ".")
-
-#test
-allvars <- allVars(nc)
-vardims <- varDims(allvars)
-df <- colbind_loc_vars(nc)
-
-mapviewOptions(platform = "leaflet")
-df %>%
-  sf::st_as_sf(coords = c("station_x_coordinate", "station_y_coordinate"), crs = 4326) %>%
-  mapview::mapview()
+# onderin warmer???
 
 
 
-VolumeVars = allVarsDims %>%
-  filter(dims == 3)
 
+ncvar_get(nc, "time")
+# lapply(vars, function(x) ncvar_get(nc, x, start = c(1,1,1), count = c(1,1,-1)))
+# #
+dff <- nc_his2df(nc, vars, station_id, layer)
+layer = 20
+dff20 <- nc_his2df(nc, vars, station_id, layer)
 
+ggplot() +
+  geom_line(data = dff, aes(datetime, value), color = "red") +
+  geom_line(data = dff20, aes(datetime, value), color = "blue")
+# 
+#  #
+# # class(station_id)
 
-ncvar_get(nc, "station_x_coordinate")
-
-locvars = c("station_x_coordinate", "station_y_coordinate",
-      "station_name", "station_id")
-
-colbind_loc_vars <- function(nc, vars){
-  
-  names(vars) = vars
-
-  l <- lapply(vars, function(x) {
-    ncdf4::ncvar_get(nc, x)
-  }) 
-  
-  if(length(unique(lapply(l, dim))) == 1){
-    df <- bind_cols(l)
-  } else stop("not all variables have the same dimension")
-
-  return(df)
-}
 
 df <- colbind_loc_vars(nc, locvars)
 
 t <- ncdf4::ncvar_get(nc, "timestep")
 
 ncdf4::ncvar_get(nc, "NH4") %>% dim()
+
+ncdf4::ncvar_get(nc, "temperature", start = c(1, , .x$time1), count = c(1, 1, -1)))
 
 ncdf4::nc_close(nc)
 
